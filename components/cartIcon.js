3427e04cd59c2b8c4a4e960edfe178b98662b88d
aryash19 <Aryashchaudhary12341234@gmail.com>
2023-07-14 00:59:50 +0530

    redux toolkit added

--- components/cartIcon.js
+++ components/cartIcon.js
@@ -2,9 +2,15 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { themeColors } from '../theme'
 import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux';
+import { selectCartItems, selectCartTotal } from '../slices/cartSlice';
 
 export default function CartIcon() {
   const navigation = useNavigation();
+  const cartItems = useSelector(selectCartItems);
+ const cartTotal = useSelector(selectCartTotal);
+
+  if (!cartItems.length) return ;
   return (
     <View className="absolute bottom-5 w-full z-50">
      <TouchableOpacity style={{backgroundColor: themeColors.bgColor(1)}}
@@ -14,14 +20,14 @@ export default function CartIcon() {
 
         <View className="p-2 px-4 rounded-full" style={{backgroundColor:'rgba(255,255,255,0.3)'}}>
         <Text className="font-extrabold text-white text-lg">
-         3   
+        {cartItems.length} 
         </Text>
         </View>
         <Text className="flex-1 text-center font-extrabold text-white text-lg">
          View Cart
         </Text>
         <Text className="font-extrabold text-white text-lg">
-        ₹ {20}
+        ₹ {cartTotal}
         </Text>
 
      </TouchableOpacity>
